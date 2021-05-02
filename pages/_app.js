49907f561fb9790401e42a5476e931e3b54267db
ICE-Cold-Ethanol <icecoldetoh@gmail.com>
2021-05-02 17:14:50 +0800

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
     const getLayout = Component.getLayout ?? ((page) => page)
 
-    return getLayout(<Component {...pageProps} />)
+    return (
+        <>
+            <Head>
+                <title>Idelon GP Tracking</title>
+            </Head>
+            {getLayout(<Component {...pageProps} />)}
+        </>
+    )
 }
 
 export default MyApp
