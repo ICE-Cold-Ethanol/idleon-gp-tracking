ee2349f1d329f68b9e18ccafb25522820185e399
ICE_Cold_Ethanol <icecoldetoh@gmail.com>
2021-05-04 14:10:57 +0800

    edited introduction

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,9 +15,10 @@ const Instruction = () => {
                 would use it to extract the data from the game.
             </p>
             <p>
-                Press F12 to open the devtools, you would see a new window pop
-                out in chrome. It should come up at the bottom in default, but
-                it can be on other side according to your personal setting.
+                <strong>Press F12</strong> to open the devtools, a new window
+                will pop out in chrome. It should come up at the bottom in
+                default, but it can be on other side according to your personal
+                setting.
             </p>
             <figure>
                 <img src="/step/chrome/1.png" alt="with devtools open" />
@@ -25,7 +26,7 @@ const Instruction = () => {
                     With devtools open, your screen should look similar to this
                 </figcaption>
             </figure>
-            <h3>Open in separate window</h3>
+            <h3>Optional: Open in separate window</h3>
             <p>
                 I would suggest you to open the devtools in a separate window so
                 you can work with it a bit easier. To do this, find the{' '}
@@ -50,13 +51,13 @@ const Instruction = () => {
 
             <h2>2. Locate Data</h2>
             <p>
-                Then you want to <strong>go in game, open the guild tab</strong>{' '}
-                which list out all the guild member and their GP, i believe you
-                are smart enough to figure out how to do it ;)
+                Then <strong>go in game and open the guild tab</strong>. I
+                believe you are smart enough to figure out how to do it ;)
             </p>
             <p>
-                Now turn back to devtools, you want to open the network tab.
-                Here you will find the records of all your network activities.
+                Back to devtools, you want to{' '}
+                <strong>open the network tab</strong>. Here you will find the
+                records of all your network activities.
             </p>
             <figure>
                 <img src="/step/chrome/3.png" alt="network tab" />
@@ -65,8 +66,7 @@ const Instruction = () => {
                 </figcaption>
             </figure>
             <p>
-                There are too much items in here, we do not care most of them,
-                so we need to filter out the one we actually need. Click the{' '}
+                We need to filter out the one we actually need. Click the{' '}
                 <strong>WS</strong> filter button in the devtools, and there
                 should be only one item left. And then click on that item{' '}
                 <strong>Name</strong> to view the detail of it.
@@ -80,16 +80,16 @@ const Instruction = () => {
             </figure>
             <p>
                 Still too much information here, we need to do some filter
-                again. This time, set the type filter from All to Receive, and
+                again. This time,{' '}
+                <strong>set the type filter from All to Receive</strong>, and
                 enter <code>_guild/.*/m</code> (exclude the backtick) into the
                 search bar.
             </p>
             <p>
-                Then there should be only one or two item left, click on anyone
-                to see the content of it, click on the small triangle to expand
-                the item content. After a few expansion, the correct one should
-                has a structure of content similar to the one shown in the
-                picture. Usually it is the lengthy one.
+                There should be few items left, you want to{' '}
+                <strong>find the one with the longer length</strong> (shown at
+                the second column), you may want to check it&apos;s content,
+                which should look similar to the one in the picture.
             </p>
             <figure>
                 <img src="/step/chrome/5.png" alt="filter setting" />
@@ -108,21 +108,23 @@ const Instruction = () => {
             </figure>
             <hr></hr>
             <h2>3. Data to Google Sheet</h2>
+            <h3>Convert Data</h3>
             <p>
                 Google Sheet is a free solution that can help use manage the
                 guild gp data. I have made a template for you to use, you can
-                get it from the header bar of this site. You should make a copy
-                to your own google drive.
+                get it from the header bar of this site. You should{' '}
+                <strong>make a copy to your own google drive</strong>.
             </p>
             <p>
                 But the raw data we copy from the devtools is not suitable to
                 use in Google Sheet, so i created a simple to to covert it for
-                you. Go to the convert section of this site, there is a big text
-                box for you to <strong>paste the raw data</strong>. Then you
-                want to click the <strong>Get Data Entries</strong> button
+                you. Go to the convert section of this site,{' '}
+                <strong>paste the raw data into the big text box</strong>. Then
+                you want to click the <strong>Get Data Entries</strong> button
                 first, if there is no error the converted data should be copied
                 to you clipboard now.
             </p>
+            <h3>Enter GP Record Data</h3>
             <p>
                 Head on to the Google Sheet and find the <em>DATA ENTRY</em>{' '}
                 sheet. Paste the data in the top left empty cell under the
@@ -137,16 +139,20 @@ const Instruction = () => {
                 </figcaption>
             </figure>
             <p>
-                And you want to paste the future data following the existing
-                data.
+                Repeat this process for future data entries. But do not
+                overwrite current records, paste them to the empty row following
+                the existing rows.
             </p>
+            <h3>Name Table</h3>
             <p>
                 We record the players using their player id, but we would want
                 to know their player name too. Back to the convert page and
                 click <strong>Get Name Table</strong>, and repeat the step but
-                this time paste to <em>NAME TABLE</em> sheet. If any player
-                changed their name, you would need to replace the data in order
-                to see their new name.
+                this time paste to <em>NAME TABLE</em> sheet.{' '}
+                <strong>
+                    You only need to do it once, or when you need to update the
+                    name in case someone change their display name.
+                </strong>
             </p>
             <figure>
                 <img src="/step/chrome/8.png" alt="paste option" />
@@ -165,22 +171,22 @@ const Instruction = () => {
                 <img src="/step/chrome/9.png" alt="paste option" />
                 <figcaption>Nice table view of the records.</figcaption>
             </figure>
+            <h3>Hightlight Unmet Requirements</h3>
             <p>
                 There is one final function that can help you to find out the
                 player that did not match the GP requirement. In the{' '}
-                <em>DATA ENTRY</em> sheet there is one green box that allow you
-                to set the target that guild members are required to get between
-                each record interval.
+                <em>DATA ENTRY</em> sheet there is a green box that allow you to
+                set the target amount of GP required between each record
+                interval.
             </p>
             <figure>
                 <img src="/step/chrome/10.png" alt="Target option" />
                 <figcaption>The GP requirement.</figcaption>
             </figure>
             <p>
-                For example lazy me did not get 20 GP for today, and the
-                requirement is 20 GP per day. The next day guild leader input
-                the data, my record would be marked in red color in the{' '}
-                <em>VIEW</em> sheet.
+                For example if I get 10 GP for today, and the requirement is 20
+                GP per day. When next day record is added, my record would be
+                marked in red color in the <em>VIEW</em> sheet.
             </p>
             <figure>
                 <img src="/step/chrome/11.png" alt="Failed requirement" />
