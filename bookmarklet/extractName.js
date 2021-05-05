69385a11c320d2c001a60669b0dee2d5544cb020
ICE_Cold_Ethanol <icecoldetoh@gmail.com>
2021-05-05 12:32:12 +0800

    added other booklets

---
+++ bookmarklet/extractName.js
@@ -0,0 +1,30 @@
+// define global var
+/* global bn, xn */
+
+;(() => {
+    // check correct hostname
+    if (window.location.hostname !== 'www.legendsofidleon.com') {
+        alert(`Stop browsing ${window.location.hostname}, go play Idleon!`)
+    }
+
+    // check data exist
+    if (!bn || !xn) {
+        alert(
+            'Guild data not fount. Please make sure that\n1. You are in a guild.\n2. You opened the guild menu.'
+        )
+    }
+
+    // map data
+    const csv = xn
+        .map((member) => {
+            // date,id,gp
+            return [member[7], member[0]].join(',')
+        })
+        .join('\n')
+
+    // to clipboard
+    window.navigator.clipboard
+        .writeText(csv)
+        .then(() => alert('Name data has been copied to your clipboard.'))
+        .catch(() => alert('Something go wrong...But it works on my machine.'))
+})()
